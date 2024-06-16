84d0bbc5de394556018be29f2230149ac857c8a2
luzede <85195923+luzede@users.noreply.github.com>
2024-06-16 13:05:06 +0300

    Base starting point for the bot using kysely, sqlite, typescript

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,6 @@ import db from "./database";
 
 import { Client, GatewayIntentBits } from "discord.js";
 
-// declare module "discord.js/typings" {
-// 	interface Client {
-// 		commands: Collection<string, Command>;
-// 	}
-// }
-
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -21,45 +15,6 @@ const client = new Client({
 		GatewayIntentBits.MessageContent,
 	],
 });
-
-// client.commands = commands;
-
-// for (const command of commands) {
-// 	client.commands.set(command.data.name, command);
-// }
-
-// client.on(Events.InteractionCreate, async (interaction) => {
-// 	console.log(interaction);
-
-// 	if (!interaction.isCommand()) return;
-// 	const command = client.commands.get(interaction.commandName);
-
-// 	if (!command) {
-// 		console.error(`No command matching ${interaction.commandName} was found.`);
-// 		return;
-// 	}
-
-// 	try {
-// 		await command.execute(interaction);
-// 	} catch (error) {
-// 		console.error(error);
-// 		if (interaction.replied || interaction.deferred) {
-// 			await interaction.followUp({
-// 				content: "There was an error while executing this command!",
-// 				ephemeral: true,
-// 			});
-// 		} else {
-// 			await interaction.reply({
-// 				content: "There was an error while executing this command!",
-// 				ephemeral: true,
-// 			});
-// 		}
-// 	}
-// });
-
-// client.once(Events.ClientReady, (readyClient) => {
-// 	console.log(`Logged in as ${readyClient.user?.tag}`);
-// });
 
 // If I had done these manually, the function in client.on(event, function) would have been an async
 // But since the function is calling an async function, I don't know if it is anti-pattern to make async function
@@ -75,22 +30,3 @@ for (const event of events) {
 }
 
 client.login(config.TOKEN);
-
-// (async () => {
-// 	await db
-// 		.insertInto("banned")
-// 		.values({
-// 			user_id: "123",
-// 			reason: "Spamming",
-// 			guild_id: "456",
-// 			ban_duration_minutes: 60,
-// 		})
-// 		.execute();
-
-// 	const banned = await db.selectFrom("banned").selectAll().execute();
-// 	console.log(banned);
-// })();
-
-// console.log(commands);
-
-// console.log(config);
